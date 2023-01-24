22ee48f9d5120ee15622e4c5aad2b8747bb416eb
jack murphy <20079445@mail.wit.ie>
2023-01-24 09:59:06 +0000

    Completed week 2 lab A

--- lab1/src/App.js
+++ lab1/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Task from './components/task';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Tasky</h1>
+      <Task title="Dishes" deadline="Today" description="Wash the dishes"/>
+      <Task title="Laundry" deadline="Tomorrow" description="Fold laundry and put away"/>
+      <Task title="Tidy" deadline="Today" description="Clean up around the house"/>
     </div>
   );
 }
 
 export default App;
+
+
